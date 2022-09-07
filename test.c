149aaca5f392c4b3fe4d7ea5b1a617d87629bb54
defu0117 <112959054+defu0117@users.noreply.github.com>
2022-09-07 22:55:04 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 int main()
 {
-	printf(" % d\n", sizeof(char));
-	printf(" % d\n", sizeof(int));
-	printf(" % d\n", sizeof(long));
-	printf(" % d\n", sizeof(long long));
-	printf(" % d\n", sizeof(float));
-	printf(" % d\n", sizeof(double));
-	return 0;
+int num1 = 0;
+int num2 = 0;
+int sum = 0;
+scanf_s ("%d%d", &num1, &num2);
+sum = num1 + num2;
+printf("sum=%d\n", sum); return 0;
 }
