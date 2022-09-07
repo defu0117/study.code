e37614a3ea2b7f0b5765aa20ce5808f067db550f
defu0117 <112959054+defu0117@users.noreply.github.com>
2022-09-07 22:31:00 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,7 +1,11 @@
-
-# include  <stdio.h>
+#include <stdio.h>
 int main()
 {
-	printf("Р§зг");
+	printf(" % d\n", sizeof(char));
+	printf(" % d\n", sizeof(int));
+	printf(" % d\n", sizeof(long));
+	printf(" % d\n", sizeof(long long));
+	printf(" % d\n", sizeof(float));
+	printf(" % d\n", sizeof(double));
 	return 0;
 }
