33b09a55485091e448dba6a19326d681d0bde1b6
defu0117 <112959054+defu0117@users.noreply.github.com>
2022-09-14 18:26:48 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,24 +1,17 @@
-//#include <stdio.h>
-//int main()
-//{
-//	int num1 = 0;
-//	int num2 = 0;
-//	int sum = 0;
-//	scanf("%d%d", &num1, &num1);
-//	sum = num1 + num2;
-//	printf("sum=%d\n", sum);
-//	return 0;
-}
 #include <stdio.h>
-int global = 2022;
 void test()
 {
-	printf("test()--%d", global);
+	static int a = 1;
+	a++;
+		printf("a=%d\n", a);
 }
 int main()
 {
-	test();
-	printf("%d", global);
+	int i = 0;
+	while (i < 5)
+	{
+		test();
+		i++;
+	}
 	return 0;
 }
-
